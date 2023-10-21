bbafed1799ae5411d2b1bb31d2d93b91b9db27b9
Yash Goyal <yashgo0018@gmail.com>
2023-10-21 18:05:20 +0100

    added some essential files

---
+++ packages/contracts/hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.20',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    polygonMumbai: {
+      url: process.env.POLYGON_MUMBAI_RPC || '',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+  },
+}
+
+export default config
