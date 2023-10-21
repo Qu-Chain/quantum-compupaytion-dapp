5bc654801d4a4d5fd1200a9a318034245ea04ec9
jan-o-e <82890641+jan-o-e@users.noreply.github.com>
2023-10-21 15:18:25 +0100

    set state for uploaded .qasm file

--- src/components/layout/FileUpload.tsx
+++ src/components/layout/FileUpload.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useRef } from 'react'
+import { ReactNode, useRef, ChangeEvent, useState } from 'react'
 import { Button, FormControl, FormErrorMessage, FormLabel, Icon, InputGroup } from '@chakra-ui/react'
 import { useForm, UseFormRegisterReturn } from 'react-hook-form'
 import { FiFile } from 'react-icons/fi'
@@ -6,55 +6,64 @@ import { FiFile } from 'react-icons/fi'
 type FileUploadProps = {
   register: UseFormRegisterReturn
   accept?: string
-  multiple?: boolean
   children?: ReactNode
 }
 
 const FileUpload = (props: FileUploadProps) => {
-  const { register, accept, multiple, children } = props
+  const { register, accept, children } = props
   const inputRef = useRef<HTMLInputElement | null>(null)
-  const { ref, ...rest } = register as {ref: (instance: HTMLInputElement | null) => void}
+  const { ref, ...rest } = register as { ref: (instance: HTMLInputElement | null) => void }
 
   const handleClick = () => inputRef.current?.click()
 
   return (
-      <InputGroup onClick={handleClick}>
-        <input
-          type={'file'}
-          multiple={multiple || false}
-          hidden
-          accept={accept}
-          {...rest}
-          ref={(e) => {
-            ref(e)
-            inputRef.current = e
-          }}
-        />
-        <>
-          {children}
-        </>
-      </InputGroup>
+    <InputGroup onClick={handleClick}>
+      <input
+        type={'file'}
+        hidden
+        accept={accept}
+        {...rest}
+        ref={(e) => {
+          ref(e)
+          inputRef.current = e
+        }}
+      />
+      <>{children}</>
+    </InputGroup>
   )
 }
 
 type FormValues = {
-  file_: FileList
+  file_: File
 }
 
 export function Upload() {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>()
-  const onSubmit = handleSubmit((data) => console.log('On Submit: ', data))
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>()
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
 
-  const validateFiles = (value: FileList) => {
+  const onSubmit = handleSubmit((data) => {
+    const selectedFile = data.file_[0]
+    console.log('On Submit: ', data)
+    console.log('Selected file: ', selectedFile)
+
+    // set the selected file to a local state variable here.
+    setSelectedFile(selectedFile)
+  })
+
+
+  const validateFiles = (value: File) => {
     if (value.length < 1) {
-      return 'Files is required'
+      return 'File is required'
     }
-    for (const file of Array.from(value)) {
-      const fsMb = file.size / (1024 * 1024)
-      const MAX_FILE_SIZE = 1
-      if (fsMb > MAX_FILE_SIZE) {
-        return 'Max file size 1mb'
-      }
+    const file = value
+    const fsMb = file.size / (1024 * 1024)
+    const MAX_FILE_SIZE = 1
+    if (fsMb > MAX_FILE_SIZE) {
+      return 'Max file size 1mb'
     }
     return true
   }
@@ -64,22 +73,14 @@ export function Upload() {
       <form onSubmit={onSubmit}>
         <FormControl isInvalid={!!errors.file_} isRequired>
           <FormLabel>{'File input'}</FormLabel>
-
           <FileUpload
-            accept=".qasm" // accepts .qasm files only
-            multiple
-            register={register('file_', { validate: validateFiles })}
-          >
-            <Button leftIcon={<Icon as={FiFile} />}>
-              Upload your QASM file
-            </Button>
+            accept='.qasm' // accepts .qasm files only
+            register={register('file_', { validate: validateFiles })}>
+            <Button leftIcon={<Icon as={FiFile} />}>Upload your QASM file</Button>
           </FileUpload>
 
-          <FormErrorMessage>
-            {errors.file_ && errors?.file_.message}
-          </FormErrorMessage>
+          <FormErrorMessage>{errors.file_ && errors?.file_.message}</FormErrorMessage>
         </FormControl>
-
         <button>Submit</button>
       </form>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <>
       <Head />
-
       <main>
         <Heading as="h2">Upload your Quantum Circuit</Heading>
         <Upload />
