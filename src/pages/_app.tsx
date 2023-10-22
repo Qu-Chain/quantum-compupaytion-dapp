842de24b030677f7567b120e0a9fecc5322d8376
jan-o-e <82890641+jan-o-e@users.noreply.github.com>
2023-10-22 03:51:52 +0100

    fixed wallet chain bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,21 +6,37 @@ import { useIsMounted } from 'hooks/useIsMounted'
 import { Seo } from 'components/layout/Seo'
 import { CircuitProvider } from 'components/layout/CircuitContext'
 
+import { WagmiConfig, createConfig } from 'wagmi'
+import { polygonMumbai, polygon, optimism, arbitrum } from 'wagmi/chains'
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
+
+const config = createConfig(
+  getDefaultConfig({
+    appName: 'ConnectKit Next.js demo',
+    //infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+    //alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    chains: [polygonMumbai, polygon, optimism, arbitrum],
+    walletConnectProjectId: '5ef7aab0f85ac724e35b3127ea6ac8fc',
+  })
+)
+
 export default function App({ Component, pageProps }: AppProps) {
   const isMounted = useIsMounted()
 
   return (
     <ChakraProvider>
       <Seo />
-      <Web3Provider>
-        <CircuitProvider>
-          {isMounted && (
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          )}
-        </CircuitProvider>
-      </Web3Provider>
+      <WagmiConfig config={config}>
+        <ConnectKitProvider>
+          <CircuitProvider>
+            {isMounted && (
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            )}
+          </CircuitProvider>
+        </ConnectKitProvider>
+      </WagmiConfig>
     </ChakraProvider>
   )
 }
